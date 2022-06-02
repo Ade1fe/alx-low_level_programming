c8cf12ce3536387e53cfc0a074f1ce9bdbb8c54c
Ade1fe <addypearl09@gmail.com>
2022-06-02 10:47:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- *  main - Does main stuff
+ *  main - Print the string in the puts function
  *
+ *Description: using main function this program prints
+ * "Programming is like building a multilingual puzzle"
  *
- * Return: Always (0) Success
+ * Return: Always 0
  * /
 int main(void)
 {
