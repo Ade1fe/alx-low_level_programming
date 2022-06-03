0cee50540980bcec583cfe5be7a453f953f677ae
Ade1fe <addypearl09@gmail.com>
2022-06-03 10:18:40 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int digit;
 
 	for (digit = 0; digit < 10; digit++)
-        putchar(digit + '0');
+	putchar(digit + '0');
 
 	putchar('\n');
 
