98b8904e9889a284a486a293c66db7354dc312e6
Ade1fe <addypearl09@gmail.com>
2022-06-03 10:25:20 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,19 +6,19 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
-	int number;
+{
+		int number;
 
-	for (number = 0; number <= 9; number++)
-	{
-	putchar(number + '0');
-	if (number < 9)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
+		for (number = 0; number <= 9; number++)
+		{
+		putchar(number + '0');
+		if (number < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+		putchar('\n');
 
 	return (0);
 }
